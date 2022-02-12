25bfd4f9c227334007150c6401249b574f07630e
Igor.Kornilov <gaber32188@gmail.com>
2022-02-12 18:26:22 +0300

    add default data and its type

---
+++ src/data/defaultProjects.ts
@@ -0,0 +1,59 @@
+import { Project } from 'types';
+
+export const projects: Project[] = [
+  {
+    created_at: '2021-11-19T13:46:36.211Z',
+    id: 'a87e8618-7392-4ac2-b4d0-c6b9b8fb3304',
+    name: 'React',
+    rating: 5,
+    url: 'https://github.com/facebook/react',
+  },
+
+  {
+    created_at: '2021-11-19T13:47:12.795Z',
+    id: 'a2f4e2e7-33ef-461c-a846-da88d6d4b536',
+    name: 'NodeJs',
+    rating: 3,
+    url: 'https://github.com/nodejs/node',
+  },
+
+  {
+    created_at: '2021-11-19T13:47:17.933Z',
+    id: '836e6ac0-4c58-4392-8795-650108e67b3c',
+    name: 'AngularJs',
+    rating: 2,
+    url: 'https://github.com/angular/angular.js?',
+  },
+
+  {
+    created_at: '2021-11-19T13:47:08.026Z',
+    id: 'b62fe100-71b6-4ce0-a8b0-3f365d99621f',
+    name: 'Django',
+    rating: 5,
+    url: 'https://github.com/django/django',
+  },
+
+  {
+    created_at: '2021-11-19T13:46:55.403Z',
+    id: '1342d001-2871-4e17-9f47-979815b825d0',
+    name: 'Jest',
+    rating: 3,
+    url: 'https://github.com/facebook/jest',
+  },
+
+  {
+    created_at: '2021-11-19T13:47:01.492Z',
+    id: '22c9861a-5c65-4275-bb15-165938896ae9',
+    name: 'Docker',
+    rating: 4,
+    url: 'https://github.com/docker',
+  },
+
+  {
+    created_at: '2021-11-19T13:46:49.460Z',
+    id: '01516ba7-0556-4c78-ba72-2a93865e5bfb',
+    name: 'Selenium',
+    rating: 1,
+    url: 'https://github.com/SeleniumHQ/selenium',
+  },
+];
---
+++ src/types/project.ts
@@ -0,0 +1,7 @@
+export interface Project {
+  created_at: string;
+  id: string;
+  name: string;
+  rating: number;
+  url: string;
+}
