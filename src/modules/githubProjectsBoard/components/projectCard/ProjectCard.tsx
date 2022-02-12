154b1421ad222e5b5db8e800ca6d70ce952d2202
Igor.Kornilov <gaber32188@gmail.com>
2022-02-12 18:48:31 +0300

    add basic projectCard component

---
+++ src/modules/githubProjectsBoard/components/projectCard/ProjectCard.tsx
@@ -0,0 +1,37 @@
+import { HTMLAttributes } from 'react';
+import cn from 'classnames';
+
+import { Project } from 'types';
+
+import { SvgIcons } from 'shared/icons';
+import { EmptyButton } from 'shared/ui';
+
+import styles from './ProjectCard.module.scss';
+
+interface ProjectCardProps extends HTMLAttributes<HTMLDivElement> {
+  project: Project;
+}
+
+export function ProjectCard(props: ProjectCardProps) {
+  const { project, className, ...htmlProps } = props;
+
+  const starsArray = Array.from({ length: project.rating });
+
+  return (
+    <div className={cn(styles.projectCard, className)} {...htmlProps}>
+      <a href={project.url} rel='noopener noreferrer' target='_blank' className={styles.cardBody}>
+        <h4 className={styles.projectName}>{project.name}</h4>
+        <ul className={styles.starList}>
+          {starsArray.map((_, index) => (
+            <li key={index}>
+              <SvgIcons.Star className={styles.starSvg} />
+            </li>
+          ))}
+        </ul>
+      </a>
+      <EmptyButton className={styles.removeButton}>
+        <SvgIcons.Cross className={styles.crosSvg} />
+      </EmptyButton>
+    </div>
+  );
+}
